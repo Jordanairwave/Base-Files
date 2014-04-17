8b354ab1a4b1485f43b6c93a5ca02ad9a60c75f7
Jordan Haines <jordan@jordanairwave.co.uk>
2014-04-17 11:39:46 +0100

    Social media tags added to header.

--- index.php
+++ index.php
@@ -11,6 +11,21 @@
 	<link href="css/style.css?v=1.0" rel="stylesheet" type="text/css" />
 	<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
 	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon"> 
+	<!-- Google -->
+	<link rel=”author” href=”https://plus.google.com/[YOUR PERSONAL G+ PROFILE HERE]“/>
+	<!-- Facebook -->
+	<meta property=”og:title” content=”[TITLE OF PAGE HERE]”/>
+	<meta property=”og:type” content=”website”/>
+	<meta property=”og:image” content=”[LINK TO THUMBNAIL MUST BE SQUARE 154PX X 154PX]”/>
+	<meta property=”og:url” content=”[URL OF CURRENT PAGE]”/>
+	<meta property=”og:description” content=”[DESCRIPTION OF PAGE]”/>
+	<meta property=”fb:admins” content=”USER_ID”/>
+	<!-- Twitter -->
+	<meta name=”twitter:card” content=”summary”>
+	<meta name=”twitter:url” content=”[URL OF CURRENT PAGE]”>
+	<meta name=”twitter:title” content=”[TITLE OF PAGE HERE]”>
+	<meta name=”twitter:description” content=”[DESCRIPTION - 200 CHARACTERS MAX LENGTH]”>
+	<meta name=”twitter:image” content=”[IMAGE TO BE SHARED, MUST BE SQUARE 154PX X 154PX]”>
 	<script>
 	  var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
 	  (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
