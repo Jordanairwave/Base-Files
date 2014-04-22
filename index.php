c20eada3fbda90ea2489683fcd662576cabdb5a9
Jordan Haines <jordan@jordanairwave.co.uk>
2014-04-22 11:28:51 +0100

    link changed to apple touch icon

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 	<meta name="description" content="" />
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link href="css/style.css?v=1.0" rel="stylesheet" type="text/css" />
-	<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+	<link rel="apple-touch-icon" href="apple-touch-icon.png" />
 	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon"> 
 	<!-- Google -->
 	<link rel=”author” href=”https://plus.google.com/[YOUR PERSONAL G+ PROFILE HERE]“/>
