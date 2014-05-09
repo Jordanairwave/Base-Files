b0cfcc5b22d24a989e352e6207bfd833f315f22a
Jordan Haines <jordan@jordanairwave.co.uk>
2014-05-09 11:52:20 +0100

    IE meta tag added to force IE to use later versions.

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 	<meta charset="utf-8">
 	<meta name="description" content="" />
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<link href="css/style.css?v=1.0" rel="stylesheet" type="text/css" />
 	<link rel="apple-touch-icon" href="apple-touch-icon.png" />
 	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon"> 
