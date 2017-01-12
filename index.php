caec1878d964a1fd62700093af71ae9392e1992d
Jordan Haines <jordanhaines@Jordans-iMac.local>
2017-01-12 17:30:12 +0000

    jquery link updated

--- index.php
+++ index.php
@@ -64,7 +64,7 @@
 			
 		</footer>
 	</div>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 	<script defer async type="text/javascript" src="js/modernizr.custom.44232.js"></script>
 	<script defer async type="text/javascript" src="js/scripts.js?v=1.0"></script>
 </body>
