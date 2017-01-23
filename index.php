54f5a3f81905c4bdd0f70d37a2311f9cb15cf342
Jordan Haines <jordanhaines@Jordans-iMac.local>
2017-01-23 11:06:57 +0000

    modernizr removed

--- index.php
+++ index.php
@@ -65,7 +65,6 @@
 		</footer>
 	</div>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-	<script defer async type="text/javascript" src="js/modernizr.custom.44232.js"></script>
 	<script defer async type="text/javascript" src="js/scripts.js?v=1.0"></script>
 </body>
 
