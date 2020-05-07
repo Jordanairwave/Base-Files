91f27a0d82a0c7a3482e551a81d8195576b57fd5
Jordan Haines <jordan@factor3.co.uk>
2020-05-07 10:05:02 +0100

    Jquery version updated to 3.5.1.min

--- index.php
+++ index.php
@@ -58,7 +58,10 @@
 			
 		</footer>
 	</div>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script
+		src="https://code.jquery.com/jquery-3.5.1.min.js"
+		integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+		crossorigin="anonymous"></script>
 	<script defer async type="text/javascript" src="js/scripts.js?v=<?php echo filemtime( 'js/scripts.js' ) ?>"></script>
 </body>
 
