7c0a6c7f4183dc6e99a92bc80bc9e2af70c97707
Jordan Haines <jordan@factor3.co.uk>
2021-05-15 12:17:41 +0100

    Index.php head updated
    Meta tags updated and old if statement code removed

--- index.php
+++ index.php
@@ -3,40 +3,28 @@
 	$pageDescription = 'Page description here';
 ?>
 <!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<html lang="en">
 <head>
 	<title><?php echo $pageTitle; ?></title>
 	<meta charset="utf-8">
 	<meta name="description" content="<?php echo $pageDescription; ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+	<meta property="og:title" content="<?php echo $pageTitle; ?>">
+	<meta property="og:description" content="<?php echo $pageDescription; ?>">
+	<meta property="og:image" content="<?php echo "http://$_SERVER[HTTP_HOST]/social.png"; ?>">
+	<meta property="og:image:alt" content="Image description">
+	<meta property="og:locale" content="en_GB">
+	<meta property="og:type" content="website">
+	<meta name="twitter:card" content="summary_large_image">
+	<meta property="og:url" content="http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]">
+	<link rel="canonical" href="https://www.mywebsite.com/page">
+
 	<link href="css/style.css?v=<?php echo filemtime( 'css/style.css' ) ?>" rel="stylesheet" type="text/css" />
 	<link rel="apple-touch-icon" href="apple-touch-icon.png" />
 	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon"> 
-
-	<!-- Facebook -->
-	<meta property=”og:title” content=<?php echo $pageTitle; ?>>
-	<meta property=”og:type” content=”website”>
-	<meta property=”og:image” content=<?php echo "http://$_SERVER[HTTP_HOST]/social.png"; ?>>
-	<meta property=”og:url” content=<?php echo "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]"; ?>>
-	<meta property=”og:description” content=<?php echo $pageDescription; ?>>
 	
-	<!-- Twitter -->
-	<meta name="twitter:card" content="summary">
-	<meta name="twitter:title" content=<?php echo $pageTitle; ?>>
-	<meta name="twitter:description" content=<?php echo $pageDescription; ?>>
-	<meta name="twitter:image:src" content=<?php echo "http://$_SERVER[HTTP_HOST]/social.png"; ?>>	
-	<meta name="twitter:url" content=<?php echo "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]"; ?>>
-	
-	<script>
-	  var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
-	  (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-	  g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-	  s.parentNode.insertBefore(g,s)}(document,'script'));
-	</script>
 </head>
 <body>
 	<div id="page-wrap">
